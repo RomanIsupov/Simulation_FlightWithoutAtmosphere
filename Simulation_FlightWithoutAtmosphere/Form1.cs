9b0cf844d4f382bd4a652555c047db8bca3aa447
RomanIsupov <romisup@gmail.com>
2021-03-02 20:31:11 +0800

    Add more precise scaling.

--- Simulation_FlightWithoutAtmosphere/Form1.cs
+++ Simulation_FlightWithoutAtmosphere/Form1.cs
@@ -20,7 +20,7 @@ namespace Simulation_FlightWithoutAtmosphere
         const double dt = 0.01;
         const double g = 9.81;
 
-        double a;
+        double alpha;
         double v0;
         double y0;
 
@@ -29,13 +29,27 @@ namespace Simulation_FlightWithoutAtmosphere
         double y;
         private void btStart_Click(object sender, EventArgs e)
         {
-            a = (double)edAngle.Value;
+            alpha = (double)edAngle.Value;
             v0 = (double)edSpeed.Value;
             y0 = (double)edHeight.Value;
 
             t = 0;
             x = 0;
             y = y0;
+
+            double maxX = (int)(v0 * v0 * Math.Sin(alpha * Math.PI / 90) / g + 1.0);
+            double maxY = (int)(y0 + v0 * v0 * Math.Pow(Math.Sin(alpha * Math.PI / 180), 2) / (2 * g) + 1.0);
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            
+            chartArea.AxisX.Minimum = 0D;
+            chartArea.AxisX.Maximum = maxX;
+            chartArea.AxisY.Minimum = 0D;
+            chartArea.AxisY.Maximum = maxY;
+            chartArea.Name = "ChartArea1";
+
+            this.chart1.ChartAreas.Clear();
+            this.chart1.ChartAreas.Add(chartArea);
+
             chart1.Series[0].Points.Clear();
             chart1.Series[0].Points.AddXY(x, y);
 
@@ -45,8 +59,8 @@ namespace Simulation_FlightWithoutAtmosphere
         private void timer1_Tick(object sender, EventArgs e)
         {
             t += dt;
-            x = v0 * Math.Cos(a * Math.PI / 180) * t;
-            y = y0 + v0 * Math.Sin(a * Math.PI / 180) * t - g * t * t / 2;
+            x = v0 * Math.Cos(alpha * Math.PI / 180) * t;
+            y = y0 + v0 * Math.Sin(alpha * Math.PI / 180) * t - g * t * t / 2;
             chart1.Series[0].Points.AddXY(x, y);
             if (y <= 0) timer1.Stop();
         }
